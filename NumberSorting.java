5b33b99d1ee7ebb87976d74bfc8954fec8f238d3
ashlesamczynski <asamczynski.laba@solvd.com>
2023-04-13 15:31:59 -0500

    completed Task 1
    quick sort algorithm implemented. Edge cases not accounted for.

--- NumberSorting.java
+++ NumberSorting.java
@@ -1,2 +1,19 @@
+import java.util.*;
+
 public class NumberSorting {
+    public static void main(String[] args){
+        Scanner kbd = new Scanner(System.in);
+        List<Integer> numList = new ArrayList<Integer>();
+
+        System.out.println("Enter integers to be sorted separated by spaces.\nEnter any letter when finished.");
+        while(kbd.hasNextInt()){
+            numList.add(kbd.nextInt());
+        }
+        int[] numArray = numList.stream().mapToInt(Integer::intValue).toArray();
+        QuickSort.quickSort(numArray);
+        System.out.print("Sorted list: ");
+        for(int i = 0; i < numArray.length; i++){
+            System.out.print(numArray[i] + " ");
+        }
+    }
 }
