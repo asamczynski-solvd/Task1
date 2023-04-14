a9c62327841bf039950583840bfdb3184c8fc33f
ashlesamczynski <asamczynski.laba@solvd.com>
2023-04-14 08:02:05 -0500

    Code Reformatted
    used Code -> Reformat Code tool on QuickSort.java and NumberSorting.java files to correct spacing and indentation. Also, added .idea/misc.xml to .gitignore file

--- NumberSorting.java
+++ NumberSorting.java
@@ -1,19 +1,19 @@
 import java.util.*;
 
 public class NumberSorting {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
         List<Integer> numList = new ArrayList<Integer>();
 
         System.out.println("Enter integers to be sorted separated by spaces.\nEnter any letter when finished.");
-        while(scanner.hasNextInt()){
+        while (scanner.hasNextInt()) {
             numList.add(scanner.nextInt());
         }
         int[] numArray = numList.stream().mapToInt(Integer::intValue).toArray();
         QuickSort.quickSort(numArray);
         System.out.print("Sorted list: ");
-        for(int i = 0; i < numArray.length; i++){
+        for (int i = 0; i < numArray.length; i++) {
             System.out.print(numArray[i] + " ");
         }
     }
